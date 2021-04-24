f62e1bbfd35ebe54fa444f2c6678080e1a1857dd
Haseeb U <haseebabbasi00@gmail.com>
2021-04-24 08:50:51 +0500

    resources issue resolved

--- lab_7/dot_9.js
+++ lab_7/dot_9.js
@@ -9,13 +9,13 @@ $(document).ready(function () {
     })
     $('#player').click(function () {
         $('#player').attr('src','resources/ryu_hadouken_pose.png')
-        $('#area').append('<img  id=id_'+powers+ ' " src="../resources/hadouken.gif"   style= "position: absolute; top: 140px; left:350px; " >')
+        $('#area').append('<img  id=id_'+powers+ ' " src="resources/hadouken.gif"   style= "position: absolute; top: 140px; left:350px; " >')
         animate('id_'+powers);
         powers++;
     })
     function animate(params) {
         console.log(params + " is going");
-        $('#'+params).animate({left: '1300px'},3000);
+        $('#'+params).animate({left: '1500px'},3000);
         // $(params).animate({left: '1000px'}, "");
     
     }
