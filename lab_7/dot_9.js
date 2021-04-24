dbd258c954dfc07119b1256ce00a56c9c5872135
Haseeb U <haseebabbasi00@gmail.com>
2021-04-24 08:46:10 +0500

    lab 7 index file

--- lab_7/dot_9.js
+++ lab_7/dot_9.js
@@ -2,14 +2,14 @@ let powers = 1;
 $(document).ready(function () {
    
     $('#player').mouseenter(function () {
-        $('#player').attr('src','/resources//ryu_animated.gif')
+        $('#player').attr('src','resources//ryu_animated.gif')
     })
     $('#player').mouseleave(function () {
-        $('#player').attr('src','/resources/ryu_stand_still.png')
+        $('#player').attr('src','resources/ryu_stand_still.png')
     })
     $('#player').click(function () {
-        $('#player').attr('src','/resources/ryu_hadouken_pose.png')
-        $('#area').append('<img  id=id_'+powers+ ' " src="/resources/hadouken.gif"   style= "position: absolute; top: 140px; left:350px; " >')
+        $('#player').attr('src','resources/ryu_hadouken_pose.png')
+        $('#area').append('<img  id=id_'+powers+ ' " src="../resources/hadouken.gif"   style= "position: absolute; top: 140px; left:350px; " >')
         animate('id_'+powers);
         powers++;
     })
