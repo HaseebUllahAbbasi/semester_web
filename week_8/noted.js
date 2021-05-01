a5092b7e4a7bedc452d52cf04e2b29b6e7c8683e
Haseeb U <haseebabbasi00@gmail.com>
2021-05-01 12:47:24 +0500

    prevent default ,stop propagation

--- week_8/noted.js
+++ week_8/noted.js
@@ -3,4 +3,13 @@
  * When an event happens on an element,
  * it first runs the handlers on it, then on its parent, then all the way up on other ancestors.
  ** /
- 
+/** 
+ * event.preventDefault()
+ * won't let you work ,
+ * stop the browser’s default behavior when performing an action
+ * 
+ * 
+ * event.stopImmediatePropagation()
+ * This method is used to prevent the propagation of an event as defined in the capturing/bubbling phase of the flow of an event in the browser.
+
+ * */
